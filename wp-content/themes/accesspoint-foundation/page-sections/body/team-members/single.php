efa4acaaa65c4636de7e7aa6ddc200dc6a9dbf57
kieranwalkerAP <kieran.walker@theaccesspoint.ltd>
2026-08-14 14:45:01 +0100

    Remove duplicate hero section inclusion in team members single page

--- wp-content/themes/accesspoint-foundation/page-sections/body/team-members/single.php
+++ wp-content/themes/accesspoint-foundation/page-sections/body/team-members/single.php
@@ -1,7 +1,7 @@
+<?php get_template_part ('page-sections/body/team-members/hero') ;?>
 
 <section class="team-members__single">
     <div class="team-members__single--inner container">
-        <?php get_template_part ('page-sections/body/team-members/hero') ;?>
         <?php get_template_part ('page-sections/body/team-members/contact-details') ;?>
     </div>
 </section>
