0c4e4de581888fe5e73cfe19451fdd0141ea112a
kieranwalkerAP <kieran.walker@theaccesspoint.ltd>
2026-08-14 11:51:03 +0100

    added the ner taxonomy to team member cards

--- wp-content/themes/accesspoint-foundation/page-sections/body/team-members/preview.php
+++ wp-content/themes/accesspoint-foundation/page-sections/body/team-members/preview.php
@@ -1,6 +1,7 @@
 <?php
-    $team_preview_image = has_post_thumbnail()? get_the_post_thumbnail_url( get_the_ID(), 'full' ): get_field( 'team_hero_image', 'option' );
-    $team_categories = get_the_terms( get_the_ID(), 'team_category' );
+	$team_preview_image = has_post_thumbnail()? get_the_post_thumbnail_url( get_the_ID(), 'full' ): get_field( 'team_hero_image', 'option' );
+	$team_categories = get_the_terms( get_the_ID(), 'team_category' );
+	$team_locations = get_the_terms( get_the_ID(), 'team_location' );
 ?>
 
 <div class="team-members__preview" data-post-id="<?php echo esc_attr( get_the_ID() ); ?>">
@@ -12,10 +13,23 @@
 
 			<?php the_title( '<h2 class="team-members__preview--title">', '</h2>' ); ?>
 
+			<?php if ( ! empty( $team_locations ) && ! is_wp_error( $team_locations ) ) : ?>
+				<div class="team-members__preview--location">
+					<?php $team_location_names = wp_list_pluck(
+							$team_locations,
+							'name'
+						);
+
+						echo esc_html(
+							implode( ', ', $team_location_names )
+						);
+					?>
+				</div>
+			<?php endif; ?>
+
 			<?php if ( ! empty( $team_categories ) && ! is_wp_error( $team_categories ) ) : ?>
 				<div class="team-members__preview--category">
-					<?php
-                        $team_category_names = wp_list_pluck(
+					<?php $team_category_names = wp_list_pluck(
                             $team_categories,
                             'name'
                         );
@@ -37,7 +51,7 @@
 
 		<div class="team-members__preview--link">
 			<a href="<?php the_permalink(); ?>" class="button button-primary-colour">
-				View <?php echo esc_html( strtok( get_the_title(), ' ' ) ); ?>
+				Meet <?php echo esc_html( strtok( get_the_title(), ' ' ) ); ?>
 			</a>
 		</div>
 	</div>
