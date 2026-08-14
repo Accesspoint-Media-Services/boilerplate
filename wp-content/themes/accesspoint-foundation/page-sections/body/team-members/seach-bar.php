34e6bccde17733bc2b13a9f09e2084dc919f7538
kieranwalkerAP <kieran.walker@theaccesspoint.ltd>
2026-08-14 12:11:16 +0100

    Remove conditional check for team search bar display

--- wp-content/themes/accesspoint-foundation/page-sections/body/team-members/seach-bar.php
+++ wp-content/themes/accesspoint-foundation/page-sections/body/team-members/seach-bar.php
@@ -1,7 +1,6 @@
-<?php if (get_field('show_team_search_bar', 'options')) : ?>
+
     <div class="team-members__search">
         <div class="team-members__search--inner container">
             <input type="text" id="team-members-search-input" placeholder="Type a person's name" autocomplete="off">
         </div>
     </div>
-<?php endif; ?>
