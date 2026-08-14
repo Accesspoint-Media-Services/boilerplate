35505abe0410fb383db9d21b2cf8b9767929bc1f
kieranwalkerAP <kieran.walker@theaccesspoint.ltd>
2026-08-14 14:43:29 +0100

    Refactor team members section structure for improved readability

--- wp-content/themes/accesspoint-foundation/page-sections/body/team-members/single.php
+++ wp-content/themes/accesspoint-foundation/page-sections/body/team-members/single.php
@@ -1,7 +1,7 @@
-<?php 
-    $email_address = get_field('email_address');
-    $phone_number = get_field('phone_number');
-?>
 
-<?php get_template_part ('page-sections/body/team-members/hero') ;?>
-<?php get_template_part ('page-sections/body/team-members/contact-details') ;?>
+<section class="team-members__single">
+    <div class="team-members__single--inner container">
+        <?php get_template_part ('page-sections/body/team-members/hero') ;?>
+        <?php get_template_part ('page-sections/body/team-members/contact-details') ;?>
+    </div>
+</section>
