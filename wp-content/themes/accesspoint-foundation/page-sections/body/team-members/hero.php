3f6a336c3399b07bcf48b811c310654b484bd940
kieranwalkerAP <kieran.walker@theaccesspoint.ltd>
2026-08-14 14:49:39 +0100

    Fix fallback image field name in hero section

--- wp-content/themes/accesspoint-foundation/page-sections/body/team-members/hero.php
+++ wp-content/themes/accesspoint-foundation/page-sections/body/team-members/hero.php
@@ -1,5 +1,5 @@
 <?php
-    $image = get_field( 'hero_image' ) ?: get_field( 'fallback_team_image', 'option' );
+    $image = get_field( 'hero_image' ) ?: get_field( 'fallback_hero_image', 'option' );
     $team_roles = implode( ', ', wp_get_post_terms( get_the_ID(), 'team_category', array( 'fields' => 'names' ) ) );
 	$team_locations = implode( ', ', wp_get_post_terms( get_the_ID(), 'team_location', array( 'fields' => 'names' ) ) );
 ?>
